7a7937dba6e786e1d6cdc86d1aa0234479337ef1
Emily J. Sherman <ejsher@umich.edu>
2017-01-18 12:01:20 -0500

    add comment read in data file

--- gapminder_analysis.R
+++ gapminder_analysis.R
@@ -5,3 +5,18 @@ ggplot(data=gapminder, aes(x= gdpPercap, y=lifeExp, color= continent)) + geom_po
 
 #make notes with hashtags
 # Author: Emily Sherman
+
+#Load necessary packages
+library(ggplot2)
+
+#Read in some realistic data
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv","gapminder-FiveYearData.csv")
+#Read in data file
+gapminder <- read.csv("gapminder-FiveYearData.csv")
+
+#Create plot with year vs life expectancy
+
+ggplot(data=gapminder, aes(x = year, y = lifeExp, color = continent)) + geom_point() + facet_grid(.~continent)
+
+#save plot
+ggsave(filename = "year_vs_lifeexp_percent.png", width = 5, height = 4, units = "in")
